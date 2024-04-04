4ac96071ef1121fd7b7f4cc3cd63b95c0b510590
Travis Aucoin <travisdevaucoin@gmail.com>
2024-04-03 21:53:43 -0500

    postgres script

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -32,7 +32,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let body = "name=test&email=test%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
-        .header("Content-Type","application/x-www-form-urlencoded")
+        .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
@@ -46,16 +46,16 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     let app_address = spawn_app();
     let client = reqwest::Client::new();
     let test_cases = vec![
-        ("name=le%guin", "missing the email"),
-        ("email=ursula_le_guin%40gmail.com","missing the name"),
-        ("","missing both name and email")
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
     ];
 
     for (invalid_body, error_message) in test_cases {
         //Act
         let response = client
             .post(&format!("{}/subscriptions", &app_address))
-            .header("Content-Type","application/x-www-form-urlencoded")
+            .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
             .await
