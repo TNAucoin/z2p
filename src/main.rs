de80ec8fb07398028ca703f72b6f9c57c42351ef
Travis Aucoin <travisdevaucoin@gmail.com>
2024-04-05 21:25:56 -0500

    added configuration for port, and db

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 
 use z2p::startup::run;
+use z2p::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("unable to bind to port 8080");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let listener = TcpListener::bind(
+        format!("127.0.0.1:{}",configuration.application_port)
+    ).expect("unable to bind to port 8080");
     run(listener)?.await
 }
